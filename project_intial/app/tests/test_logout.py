763c855eff84416f693b763c7cd9638f1efa5668
Mkellia <144325796+Mkellia@users.noreply.github.com>
2024-11-25 15:48:25 +0200

    test logout

--- project_intial/app/tests/test_logout.py
+++ project_intial/app/tests/test_logout.py
@@ -1,1 +1,8 @@
+from selenium.webdriver.common.by import By
+
+# Test: Logout Functionality
+def test_logout(driver):
+    driver.get("http://127.0.0.1:5000/dashboard")
+    driver.find_element(By.ID, "logout").click()
+    assert "Login" in driver.page_source, "Logout failed"
 
